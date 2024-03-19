3793cc180bb0b75f2684323826e818feeb691931
Mohamed Ashraf <gexof01@gmail.com>
2024-03-19 04:14:44 +0200

    platform module

---
+++ src/app/platform/platform.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlatformRoutingModule } from './platform-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlatformRoutingModule
+  ]
+})
+export class PlatformModule { }
