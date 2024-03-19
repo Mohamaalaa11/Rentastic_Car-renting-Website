e31133ea76fbbbec27ef4e6b3c61bd656b258b58
Mohamed Ashraf <gexof01@gmail.com>
2024-03-19 03:30:43 +0200

    routing and form group for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
-import { HeroComponent } from './home/hero/hero.component';
-import { FilterComponent } from './home/filter/filter.component';
-import { SiteInfoComponent } from './home/site-info/site-info.component';
-import { CardComponent } from './Components/card/card.component';
-import { LoginComponent } from './auth/components/login/login.component';
-
-import { MyOrdersComponent } from './user/my-orders/my-orders.component';
-import { ProfileComponent } from './user/profile/profile.component';
-import { OrderCardComponent } from './user/my-orders/order-card/order-card.component';
-
-import { RegisterComponent } from './auth/components/register/register.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavBarComponent,
-    HeroComponent,
-    FilterComponent,
-    SiteInfoComponent,
-    CardComponent,
-
-    LoginComponent,ProfileComponent,MyOrdersComponent,OrderCardComponent,
-    
-
-    
-    RegisterComponent,
-
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  imports: [BrowserModule, AppRoutingModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl<string>('', [Validators.required]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  });
 
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
