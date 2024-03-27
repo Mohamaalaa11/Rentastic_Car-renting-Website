cbd40553bf8b3774e452efc3a909cf85d0ab3208
Mohamed Ashraf <gexof01@gmail.com>
2024-03-27 03:55:35 +0200

    fix some imports

--- src/app/platform/admin/admin.module.ts
+++ src/app/platform/admin/admin.module.ts
@@ -5,12 +5,23 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { ListcarComponent } from './components/listcar/listcar.component';
 import { AdminhomeComponent } from './components/adminhome/adminhome.component';
 import { AddcarComponent } from './components/addcar/addcar.component';
-import { AppRoutingModule } from '../../app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReservationlistComponent } from './components/reservationlist/reservationlist.component';
+import { EditcarComponent } from './components/editcar/editcar.component';
+import { DeletecarComponent } from './components/deletecar/deletecar.component';
+import { DeleteConfirmationComponentComponent } from './components/delete-confirmation-component/delete-confirmation-component.component';
 
 @NgModule({
-  declarations: [ListcarComponent, AdminhomeComponent, AddcarComponent],
+  declarations: [
+    ListcarComponent,
+    AdminhomeComponent,
+    AddcarComponent,
+    ReservationlistComponent,
+    EditcarComponent,
+    DeletecarComponent,
+    DeleteConfirmationComponentComponent,
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
