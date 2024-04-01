060454a2cd3b9547b8896f95fe27c7b845e49bb8
Mohamed Ashraf <gexof01@gmail.com>
2024-04-02 01:37:34 +0200

    done filtering using dates and car brand

--- src/app/platform/home/components/filter/filter.component.ts
+++ src/app/platform/home/components/filter/filter.component.ts
@@ -1,53 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Car } from '../../../../Car';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CarRentDetailsComponent } from '../../../car-rental/components/car-rent-details/car-rent-details.component';
 import { CarentalServiceService } from '../../../admin/Services/carental-service.service';
 import { RouteReuseStrategy, Router } from '@angular/router';
-
+import { CarService } from '../../../car-rental/services/car.service';
+import { Observable } from 'rxjs';
+import { CarFilter } from '../../../car-rental/types/car-filter';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  styleUrls: ['./filter.component.css'],
 })
-export class FilterComponent {
-  filteredCars: any[] = [];
+export class FilterComponent implements OnInit {
+  carBrand$: Observable<string[]> = this.carServices.carsBrand$;
 
-  constructor(private carService: CarentalServiceService, private router: Router) {}
+  constructor(private router: Router, private carServices: CarService) {}
+
+  ngOnInit(): void {
+    this.carServices.getCarsBrand();
+  }
 
   form = new FormGroup({
-    cartype: new FormControl<string>(''),
+    carBrand: new FormControl<string>(''),
     startDate: new FormControl<Date>(new Date()),
-    endDate: new FormControl<Date>(new Date())
+    endDate: new FormControl<Date>(new Date()),
   });
 
-  cartypes: string[] = ["BMW", "Audi", "Nissan", "Lamborghini"];
+  onFilterByDate() {
+    const startDate: Date = new Date(this.form.value.startDate!);
+    const endDate: Date = new Date(this.form.value.endDate!);
 
-  GetData() {
-    const requestBody = {
-      startYear: this.form.value.startDate!.getFullYear().toString(),
-      startMonth: (this.form.value.startDate!.getMonth() + 1).toString(),
-      startDay: this.form.value.startDate!.getDate().toString(),
-      endYear: this.form.value.endDate!.getFullYear().toString(),
-      endMonth: (this.form.value.endDate!.getMonth() + 1).toString(),
-      endDay: this.form.value.endDate!.getDate().toString()
+    const queryParams: CarFilter = {
+      brand: this.form.controls.carBrand.value!,
+      startYear: startDate!.getFullYear().toString(),
+      startMonth: (startDate!.getMonth() + 1).toString(),
+      startDay: startDate!.getDate().toString(),
+      endYear: endDate!.getFullYear().toString(),
+      endMonth: (endDate!.getMonth() + 1).toString(),
+      endDay: endDate!.getDate().toString(),
     };
-  
-    this.carService.getCarsAvailability(requestBody).subscribe(
-      response => {
-        console.log(response);
-        this.filteredCars = response;
-  
-        if (this.form.value.cartype !== '') {
-          this.filteredCars = this.filteredCars.filter(car => car.Brand === this.form.value.cartype);
-        }
-        this.router.navigate(['/filtered-cars'], { state: { filteredCars: this.filteredCars } });
-      },
-      error => {
-        console.error(error);
-      }
-    );
+
+    this.router.navigate(['/car-rental', 'cars'], { queryParams: queryParams });
+
+    console.log(queryParams);
+
+    return false;
   }
-}  
+
+  // GetData() {
+  //   const requestBody = {
+  //     startYear: this.form.value.startDate!.getFullYear().toString(),
+  //     startMonth: (this.form.value.startDate!.getMonth() + 1).toString(),
+  //     startDay: this.form.value.startDate!.getDate().toString(),
+  //     endYear: this.form.value.endDate!.getFullYear().toString(),
+  //     endMonth: (this.form.value.endDate!.getMonth() + 1).toString(),
+  //     endDay: this.form.value.endDate!.getDate().toString(),
+  //   };
+
+  //   this.carService.getCarsAvailability(requestBody).subscribe(
+  //     (response) => {
+  //       console.log(response);
+  //       this.filteredCars = response;
+
+  //       if (this.form.value.cartype !== '') {
+  //         this.filteredCars = this.filteredCars.filter(
+  //           (car) => car.Brand === this.form.value.cartype
+  //         );
+  //       }
+  //       this.router.navigate(['/filtered-cars'], {
+  //         state: { filteredCars: this.filteredCars },
+  //       });
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     }
+  //   );
+  // }
+}
