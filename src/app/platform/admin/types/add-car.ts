01f4f84ada9dbcf18fb28c826b83cd7797a4ab24
Mohamed Ashraf <gexof01@gmail.com>
2024-04-05 21:35:52 +0200

    upload car image to firebase and save the link to db

---
+++ src/app/platform/admin/types/add-car.ts
@@ -0,0 +1,13 @@
+export interface AddCar {
+  Name: string;
+  Brand: string;
+  ModelYear: string;
+  Description: string;
+  Color: string;
+  Category: string;
+  SeatCount: number;
+  PricePerDay: number;
+  Images: string;
+  HasAirCondition: boolean;
+  IsAutomatic: boolean;
+}
