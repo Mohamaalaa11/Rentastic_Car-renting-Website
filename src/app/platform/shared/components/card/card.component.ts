e2a615d0ddfc6065346d16c3e2e92bc2f3135d56
Mohamed Alaa <Mohamed Alaa@DESKTOP-SD0GMEP>
2024-04-06 15:14:13 +0200

    car review

--- src/app/platform/shared/components/card/card.component.ts
+++ src/app/platform/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Car } from '../../../car-rental/types/car';
 
 @Component({
@@ -6,6 +6,17 @@ import { Car } from '../../../car-rental/types/car';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   @Input({}) car?: Car;
+   avgRate :number =0
+
+   
+  ngOnInit(): void {
+    
+    if (this.car && this.car.Reviews && this.car.Reviews.length > 0) {
+      const totalRatings = this.car.Reviews.reduce((sum, review) => sum + review.Rate, 0);
+      this.avgRate = totalRatings / this.car.Reviews.length;
+    }
+  }
+
 }
